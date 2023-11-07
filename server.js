a9b736b4db87688d0cefcfef9f8a9ea5398dc907
Marcel Langr <langr@exec.cz>
2023-11-07 09:14:39 +0100

    Final version

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
-
+const people = require('./people.json');
 const app = express();
 
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Brave World!');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello Brave World!');
+  res.render('index',{
+    title: 'Home Page',
+    people: people.profiles
+  });
+});
+
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find(p => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
 });
 
 const server = app.listen(7000, () => {
